4f1e3b6b0da53c96b334dd1b720c535cbb016ab6
sizzlei <sizzlei@icloud.com>
2019-11-20 16:26:45 +0900

    Service Create

--- __main__.py
+++ __main__.py
@@ -3,6 +3,7 @@ import BirdNest
 import time
 import sys
 import threading
+import os
 
 # Module
 netCnf = BirdNest.configure('bird.cnf')
@@ -100,3 +101,41 @@ def Birdhouse():
     # Thread Start
     threadMommy.start()
     threadDaddy.start()
+
+
+pidFile = '.mariaBird.pid'
+if __name__ == "__main__":
+    if sys.argv[1] == 'start':
+        try:
+            f = open(pidFile,'r')
+            pid = f.read()
+            f.close()
+            if pid:
+                print("already Running Process PID : " + str(pid))
+        except:
+            f = open(pidFile,'w')
+            pid = os.getpid()
+            f.write(str(pid))
+            f.close()
+
+            # Start MariaBird
+            Birdhouse()
+    elif sys.argv[1] == 'stop':
+        try:
+            f = open(pidFile,'r')
+            pid = f.read()
+            f.close()
+
+            killCmd = 'kill ' + pid
+            os.system(killCmd)
+        except:
+            print("Check for Bird PID file.")
+    elif sys.argv[1] == 'status':
+        try:
+            f = open(pidFile, 'r')
+            pid = f.read()
+            f.close()
+
+            print("PID : " + str(pid))
+        except:
+            print("Not Running MariaBird.")
